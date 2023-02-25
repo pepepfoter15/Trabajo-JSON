6bbfea150c61d71de95b0954ef1912562d65d921
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-02-25 17:59:33 +0100

    Update Proyecto_ejercicio.py

---
+++ Proyecto_ejercicio.py
@@ -0,0 +1,25 @@
+from Proyecto_funciones import *
+
+with open('Proyecto JSON.json', 'r') as f:
+    lista_enbruto = f.readlines()
+
+lista=[]
+
+
+opcionelegida=0
+
+while opcionelegida != 6:
+    opcionelegida=menu()
+    if opcionelegida == 1:
+        opción1 =listar_informacion(lista)
+    elif opcionelegida == 2:
+        opción2=contar_informacion(lista)
+    elif opcionelegida == 3:
+        opción3 =buscar_filtar_informacion(lista)
+    elif opcionelegida == 4:
+        opción4 =buscar_informacion_relacionada(lista)
+    elif opcionelegida == 5:
+        opción5 =intercambiar_autor(lista)
+
+if opcionelegida == 6:
+    print("Usted ha salido del programa.")
