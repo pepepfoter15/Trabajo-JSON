1bfdc886dbdd10ceaa9ef37f03865cea643fea00
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-02-27 16:06:27 +0100

    Update Proyecto_funciones.py

--- Proyecto_funciones.py
+++ Proyecto_funciones.py
@@ -39,15 +39,24 @@ def contar_informacion(premionobeles):
 
     print("Hay ",contador_chemistry,"premios de química.")
     print("Hay ",contador_peace,"premios de paz.")
-
-
-
-
-def buscar_filtar_informacion():
+    
+def buscar_filtar_informacion(premionobeles):
     print("Filtrar pidiendo el año y la categoría y que muestre todas las caracterías de los galardonados o premiados (laureates).")
+    año_pedido = input("Introduce el año que quiere saber los premios nobeles: ")
+    categoria_pedida = input("Introduce la categoría que quiera saber su información: ")
+    for premiados in premionobeles["prizes"]:
+        if año_pedido == premiados["year"] and categoria_pedida == premiados["category"]:
+            for informacion in premiados["laureates"]:
+                print("El id es", informacion["id"],", el nombre es",informacion["firstname"],", el motivo es",informacion["motivation"]," y la entrega es",informacion["share"],".")
 
-def buscar_informacion_relacionada():
+def buscar_informacion_relacionada(premionobeles):
     print("Pedir el id del autor y que nos muestre todo sobre ese id, el año y la categoría (su nombre, su motivo (motivation) y la parte de ese mismo (share)).")
+    id_autor_pedido = input("Introduzca el id del autor que quiera ver todo lo que consiguió: ")
+    for premiados in premionobeles["prizes"]:
+        for informacion in premiados["laureates"]:
+            if id_autor_pedido == informacion["id"]:
+                print("El nombre del autor es",informacion["firstname"],"que ganó el premio nobel de",premiados["category"],"el año",premiados["year"],". La principal motivación de este autor fue",informacion["motivation"],". PD: la entrega es",informacion["share"])
 
+                
 def intercambiar_autor():
     print("Pidiendo el id de la persona que quieres cambiar y el id del que quieres remplazar, intercambiar el nombre y el apellido de ellos.")
