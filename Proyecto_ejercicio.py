080623adf6026b7fc8d98ee032e1358cb89ce2d0
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-02-27 13:55:28 +0100

    Cambios en este repositorio

--- Proyecto_ejercicio.py
+++ Proyecto_ejercicio.py
@@ -1,26 +1,21 @@
 #EJERCICIO PROYECTO JSON
 from Proyecto_funciones import *
-
-with open('Proyecto JSON.json', 'r') as f:
-    lista_enbruto = f.readlines()
-
-lista=[]
-
+import json
 
 opcionelegida=0
 
 while opcionelegida != 6:
-    opcionelegida=menu()
+    opcionelegida=menu(opcionelegida)
     if opcionelegida == 1:
-        opción1 =listar_informacion(lista)
+        opción1 =listar_informacion(premiosnobeles)
     elif opcionelegida == 2:
-        opción2=contar_informacion(lista)
+        opción2=contar_informacion(premiosnobeles)
     elif opcionelegida == 3:
-        opción3 =buscar_filtar_informacion(lista)
+        opción3 =buscar_filtar_informacion(premiosnobeles)
     elif opcionelegida == 4:
-        opción4 =buscar_informacion_relacionada(lista)
+        opción4 =buscar_informacion_relacionada(premiosnobeles)
     elif opcionelegida == 5:
-        opción5 =intercambiar_autor(lista)
+        opción5 =intercambiar_autor(premiosnobeles)
 
 if opcionelegida == 6:
     print("Usted ha salido del programa.")
